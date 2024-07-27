e71bf8d45bd34a46b4f2a3dfda39befd09ef827c
Nikita Ryabinskii <ryabinskiyna@gmail.com>
2024-07-27 11:06:17 +0300

    add 2 number experiments

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,33 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
+        Scanner scanner = new Scanner(System.in);
 
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        System.out.print("Введите первое число: ");
+        int firstNumber = scanner.nextInt();
+
+        System.out.print("Введите второе число: ");
+        int secondNumber = scanner.nextInt();
+
+        int sum = firstNumber + secondNumber;
+        int difference = firstNumber - secondNumber;
+        int product = firstNumber * secondNumber;
+
+        double quotient;
+        if (secondNumber != 0) {
+            quotient = (double) firstNumber / secondNumber;
+        } else {
+            System.out.println("Ошибка: Деление на ноль невозможно.");
+            quotient = Double.NaN;
+        }
+
+        System.out.println("Сумма: " + sum);
+        System.out.println("Разность: " + difference);
+        System.out.println("Произведение: " + product);
+        if (!Double.isNaN(quotient)) {
+            System.out.println("Частное: " + quotient);
+        }
+
+        scanner.close();
     }
 }

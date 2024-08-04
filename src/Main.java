bb8c80346a78226d0f6a1fcb733df737116e58c7
Nikita Ryabinskii <ryabinskiyna@gmail.com>
2024-08-04 13:49:10 +0300

    added a while loop that will ask the console for the path to the file

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,27 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
+        Scanner scanner = new Scanner(System.in);
+        int fileCounter = 0;
 
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        while (true) {
+            System.out.println("Введите путь к файлу:");
+            String path = scanner.nextLine();
+            File file = new File(path);
+
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+
+            if (!fileExists || isDirectory) {
+                System.out.println("Указанный файл не существует или путь ведет к папке.");
+                continue;
+            }
+
+            fileCounter++;
+            System.out.println("Путь указан верно");
+            System.out.println("Это файл номер " + fileCounter);
+        }
     }
 }

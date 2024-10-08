742c121c254dbd6e31bc0d2ccfc46767b0b22297
Nikita Ryabinskii <ryabinskiyna@gmail.com>
2024-10-09 01:12:03 +0300

    Task №1. Exception handling

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int fileCounter = 0;
@@ -22,6 +25,39 @@ public class Main {
             fileCounter++;
             System.out.println("Путь указан верно");
             System.out.println("Это файл номер " + fileCounter);
+
+            try {
+                FileReader fileReader = new FileReader(path);
+                BufferedReader reader = new BufferedReader(fileReader);
+                String line;
+                int totalLines = 0;
+                int maxLength = Integer.MIN_VALUE;
+                int minLength = Integer.MAX_VALUE;
+
+                while ((line = reader.readLine()) != null) {
+                    int length = line.length();
+
+                    if (length > 1024) {
+                        throw new LineTooLongException("Ошибка: Строка длиннее 1024 символов в файле: " + path);
+                    }
+
+                    totalLines++;
+                    if (length > maxLength) {
+                        maxLength = length;
+                    }
+                    if (length < minLength) {
+                        minLength = length;
+                    }
+                }
+                System.out.println("Общее количество строк в файле: " + totalLines);
+                System.out.println("Длина самой длинной строки: " + maxLength);
+                System.out.println("Длина самой короткой строки: " + minLength);
+            } catch (LineTooLongException ex) {
+                System.out.println(ex.getMessage());
+                break;
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
         }
     }
 }
